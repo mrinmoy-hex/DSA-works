da0babec7609dcf18ea7ab0ac89631d7061ac030
mrinmoy-hex <init2350@gmail.com>
2026-05-22 19:36:16 +0530

    add quick sort algo

--- quick_sort/algo.c
+++ quick_sort/algo.c
@@ -1,6 +1,80 @@
 #include <stdio.h>
 
+void printArr(int *arr, int n)
+{
+    // Print array elements
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
 
-int main() {
+int partition(int *arr, int low, int high)
+{
+    int pivot = arr[low]; // choose first element as pivot
+    int i = low + 1;      // left pointer
+    int j = high;         // right pointer
+    int temp;
+
+    do
+    {
+        // move i until larger element is found
+        while (arr[i] <= pivot)
+        {
+            i++;
+        }
+
+        // move j until smaller element is found
+        while (arr[j] > pivot)
+        {
+            j--;
+        }
+
+        // swap misplaced elements
+        if (i < j)
+        {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+    } while (i < j);
+
+    // place pivot in its correct position
+    temp = arr[low];
+    arr[low] = arr[j];
+    arr[j] = temp;
+
+    return j; // return pivot index
+}
+
+void quickSort(int *arr, int low, int high)
+{
+    int partitionIndex;
+
+    // continue if subarray has more than one element
+    if (low < high)
+    {
+        partitionIndex = partition(arr, low, high);
+
+        quickSort(arr, low, partitionIndex - 1);  
+        quickSort(arr, partitionIndex + 1, high);
+    }
+}
+
+int main()
+{
+    int arr[] = {3, 5, 2, 13, 12, 3, 45};
+    int n = 7;
+
+    printf("Array before sorting:\n");
+    printArr(arr, n);
+
+    printf("Array after quick sort:\n");
+    quickSort(arr, 0, n - 1);
+
+    printArr(arr, n);
+
     return 0;
 }
